df044f8255983c03d4e26ed5e5f65b697176ebcb
Lakhan Samani <lakhan.m.samani@gmail.com>
2023-07-12 23:23:30 +0530

    Add validate_session

---
+++ examples/validate_session.go
@@ -0,0 +1,29 @@
+package examples
+
+import (
+	"fmt"
+
+	"github.com/authorizerdev/authorizer-go"
+)
+
+// ValidateSessionExample demonstrates how to use ValidateSession function of authorizer sdk
+func ValidateSessionExample() {
+	c, err := authorizer.NewAuthorizerClient(ClientID, AuthorizerURL, "", nil)
+	if err != nil {
+		panic(err)
+	}
+	_, err = c.Login(&authorizer.LoginInput{
+		Email:    "test@yopmail.com",
+		Password: "Abc@123",
+	})
+	if err != nil {
+		panic(err)
+	}
+	res, err := c.ValidateSession(&authorizer.ValidateSessionInput{
+		Cookie: "", // TODO set cookie here
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res.IsValid)
+}
---
+++ validate_session.go
@@ -0,0 +1,36 @@
+package authorizer
+
+import "encoding/json"
+
+// ValidateSessionInput defines attributes for validate_session request
+type ValidateSessionInput struct {
+	Cookie string    `json:"token,omitempty"`
+	Roles  []*string `json:"roles,omitempty"`
+}
+
+// ValidateSessionResponse defines attributes for validate_session response
+
+type ValidateSessionResponse struct {
+	IsValid bool `json:"is_valid"`
+}
+
+// ValidateSession is method attached to AuthorizerClient.
+// It performs validate_session query on authorizer instance.
+// It returns ValidateSessionResponse reference or error.
+// For implementation details check ValidateSessionExample examples/validate_session.go
+func (c *AuthorizerClient) ValidateSession(req *ValidateSessionInput) (*ValidateSessionResponse, error) {
+	bytesData, err := c.ExecuteGraphQL(&GraphQLRequest{
+		Query: `query validateSession($data: ValidateSessionInput!){validate_session(params: $data) { is_valid } }`,
+		Variables: map[string]interface{}{
+			"data": req,
+		},
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res map[string]*ValidateSessionResponse
+	json.Unmarshal(bytesData, &res)
+
+	return res["validate_session"], nil
+}
